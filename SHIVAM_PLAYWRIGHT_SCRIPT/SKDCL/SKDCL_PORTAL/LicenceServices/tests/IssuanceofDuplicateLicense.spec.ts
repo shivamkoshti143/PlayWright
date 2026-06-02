da1e34c1ab41a8900325a8e5874d712b98affe53
shivamkoshti-abm <shivam.koshti@abmindia.com>
2026-06-02 17:09:50 +0530

    Issuance of Duplicate License

---
+++ SHIVAM_PLAYWRIGHT_SCRIPT/SKDCL/SKDCL_PORTAL/LicenceServices/tests/IssuanceofDuplicateLicense.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test';
+import { doLogin, selectFile } from './Login.spec';
+
+test('IssuanceofDuplicateLicense', async ({ page }) => {
+  test.setTimeout(0);
+  await doLogin(page);
+
+  await page.getByRole('link', { name: 'Citizen Services' }).click();
+  await page.getByRole('link', { name: 'License ' }).click();
+  await page.getByRole('link', { name: 'Issuance of Duplicate License' }).click();
+
+  await page.getByRole('textbox', { name: ': License Number *' }).click();
+  await page.getByRole('textbox', { name: ': License Number *' }).fill('KT1100');
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  await page.getByRole('button', { name: 'Proceed' }).click();
+
+
+  await page.getByRole('radio', { name: 'Online' }).check();
+
+
+  await page.getByRole('button', { name: 'Submit' }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('button', { name: 'Yes' }).click();
+
+
+  const page1 = await page1Promise;
+  await page.getByRole('button', { name: 'Proceed' }).click();
+
+  await page.locator('#paymobile2').click();
+  await page.locator('#paymobile2').fill('9157285139');
+
+
+  await page.locator('#payeemail2').click();
+  await page.locator('#payeemail2').fill('sample@gmail.com');
+
+
+  await page.locator('#cbBankid').selectOption('2');
+  await page.getByRole('button', { name: 'Pay' }).click();
+  await page.locator('#tab-4').check();
+  await page.locator('#citrusAvailableOptions').selectOption('2001:000000');
+
+  const [paymentPage] = await Promise.all([
+    await page.getByRole('button', { name: 'Make Payment' }).click(),
+
+    await page.getByRole('textbox', { name: 'Mobile Number' }).click(),
+    await page.getByRole('textbox', { name: 'Mobile Number' }).fill('9157285139'),
+    await page.getByRole('textbox', { name: 'Email Id' }).click(),
+    await page.getByRole('textbox', { name: 'Email Id' }).fill('sample@gmail.com'),
+    await page.locator('#bankID').selectOption('2001'),
+    await page.getByRole('button', { name: 'Pay Now' }).click(),
+    await page.getByRole('button', { name: 'Click To Complete Transaction' }).click(),
+
+  ]);
+});
