5c79e05248a5c71013be5b68494ad9d665ac9b25
shivamkoshti-abm <shivam.koshti@abmindia.com>
2026-06-03 18:23:18 +0530

    Marriage Registration

--- SHIVAM_PLAYWRIGHT_SCRIPT/SKDCL/SKDCL_PORTAL/MarriageRegistration/tests/MarriageRegistration.spec.ts
+++ SHIVAM_PLAYWRIGHT_SCRIPT/SKDCL/SKDCL_PORTAL/MarriageRegistration/tests/MarriageRegistration.spec.ts
@@ -7,12 +7,189 @@ test('MarriageRegistration', async ({ page }) => {
 
   await page.getByRole('link', { name: 'Citizen Services' }).click();
   await page.getByRole('link', { name: 'Marriage Registration ' }).click();
-  await page.getByRole('link', { name: 'Marriage Registration' }).click();
+  await page.getByRole('link', { name: 'Marriage Registration', exact: true }).click();
+  await page.getByRole('textbox', { name: ': First Name *' }).click();
+  await page.getByRole('textbox', { name: ': First Name *' }).fill('Prakash');
+  await page.getByRole('textbox', { name: ': Middle Name' }).click();
+  await page.getByRole('textbox', { name: ': Middle Name' }).fill('Rajesh');
+  await page.getByRole('textbox', { name: ': Last Name *' }).click();
+  await page.getByRole('textbox', { name: ': Last Name *' }).fill('Koshti');
+  await page.locator('#mobileNo').click();
+  await page.locator('#mobileNo').fill('8208487562');
+  await page.locator('#emailId').click();
+  await page.locator('#emailId').fill('shivamkoshti1@gmail.com');
+  await page.locator('#areaName').click();
+  await page.locator('#areaName').fill('Andheri');
+  await page.locator('#pinCode').click();
+  await page.locator('#pinCode').fill('400392');
+  await page.getByLabel('WARD').selectOption('806');
+  await page.getByRole('textbox', { name: 'DD/MM/YYYY' }).click();
+  await page.getByRole('link', { name: '1', exact: true }).click();
+  await page.getByRole('textbox', { name: ': Place of Marriage (with full particulars) *' }).click();
+  await page.getByRole('textbox', { name: ': Place of Marriage (with full particulars) *' }).fill('Andheri');
+  await page.getByRole('textbox', { name: ': Place of Marriage (with full particulars) *' }).click();
+  await page.getByRole('textbox', { name: ': Place of Marriage (with full particulars) *' }).press('ArrowLeft');
 
+  await page.locator('#personalLaw_chosen a').filter({ hasText: 'Select' }).click();
+  await page.locator('#personalLaw_chosen').getByText('Hindu').click();
+  await page.getByRole('textbox', { name: ': Priest Name ( in English ) *' }).click();
+  await page.getByRole('textbox', { name: ': Priest Name ( in English ) *' }).fill('Bhagavat Pandit');
+  await page.getByRole('textbox', { name: ': Priest Name ( in English ) *' }).click();
+  await page.getByRole('textbox', { name: ': Priest Name ( in English ) *' }).press('ArrowLeft');
+
+  await page.getByRole('textbox', { name: ': Full Address *' }).click();
+  await page.getByRole('textbox', { name: ': Full Address *' }).fill('Bandra');
+  await page.getByRole('textbox', { name: ': Full Address *' }).click();
+  await page.getByRole('textbox', { name: ': Full Address *' }).press('ArrowLeft');
+
+  await page.locator('a').filter({ hasText: 'Select' }).click();
+  await page.locator('#priestReligion_chosen').getByText('Hindu').click();
+  await page.getByRole('textbox', { name: ': Age *' }).click();
+  await page.getByRole('textbox', { name: ': Age *' }).fill('35');
+  await page.getByRole('button', { name: 'Save & Next' }).click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await page.getByRole('textbox', { name: ': First Name (in English ) *' }).click();
+  await page.getByRole('textbox', { name: ': First Name (in English ) *' }).fill('Prakash');
+  await page.getByRole('textbox', { name: ': First Name (in English ) *' }).click();
+  await page.getByRole('textbox', { name: ': First Name (in English ) *' }).press('ArrowLeft');
+
+  await page.getByRole('textbox', { name: ': Middle Name ( in English )' }).click();
+  await page.getByRole('textbox', { name: ': Middle Name ( in English )' }).fill('Rajesh');
+  await page.getByRole('textbox', { name: ': Middle Name ( in English )' }).click();
+  await page.getByRole('textbox', { name: ': Middle Name ( in English )' }).press('ArrowLeft');
+  await page.getByRole('textbox', { name: ': Middle Name ( in English )' }).click();
   
+  await page.getByRole('textbox', { name: ': Last Name ( in English) *' }).click();
+  await page.getByRole('textbox', { name: ': Last Name ( in English) *' }).fill('Koshti');
+  await page.getByRole('textbox', { name: ': Last Name ( in English) *' }).click();
+  await page.getByRole('textbox', { name: ': Last Name ( in English) *' }).press('ArrowLeft');
+  await page.locator('#uidNo').click();
+  await page.locator('#uidNo').fill('979894059164');
+  
+  await page.getByRole('textbox', { name: ': Date Of Birth *' }).click();
+  await page.getByRole('combobox').nth(3).selectOption('1994');
+  await page.getByRole('combobox').nth(2).selectOption('6');
+  await page.getByRole('link', { name: '16' }).click();
+  await page.locator('#religionBirth_chosen a').filter({ hasText: 'Select' }).click();
+  await page.locator('#religionBirth_chosen').getByText('Hindu').click();
+  await page.locator('#occupation_chosen a').filter({ hasText: 'Select' }).click();
+  await page.locator('#occupation_chosen').getByRole('listitem').filter({ hasText: 'Not Stated' }).click();
+  await page.locator('#statusMarTime_chosen a').filter({ hasText: 'Select' }).click();
+  await page.locator('#statusMarTime_chosen').getByText('Unmarried').click();
+  await page.getByRole('textbox', { name: ': Husband Full Address *' }).click();
+  await page.getByRole('textbox', { name: ': Husband Full Address *' }).fill('Andheri');
+  await page.getByRole('textbox', { name: ': Husband Full Address *' }).click();
+  await page.getByRole('textbox', { name: ': Husband Full Address *' }).press('ArrowLeft');
+  await page.getByRole('button', { name: 'Save & Next' }).click();
 
 
+  await page.locator('input[name="marriageDTO.wifeDTO.firstNameEng"]').click();  
+  await page.locator('input[name="marriageDTO.wifeDTO.firstNameEng"]').fill('Jaya');
+  await page.locator('input[name="marriageDTO.wifeDTO.firstNameEng"]').click();
+  await page.locator('input[name="marriageDTO.wifeDTO.firstNameEng"]').press('ArrowLeft');
 
+  await page.locator('input[name="marriageDTO.wifeDTO.middleNameEng"]').click();
+  await page.locator('input[name="marriageDTO.wifeDTO.middleNameEng"]').fill('Arvind');
+  await page.locator('input[name="marriageDTO.wifeDTO.middleNameEng"]').click();
+  await page.locator('input[name="marriageDTO.wifeDTO.middleNameEng"]').press('ArrowLeft');
+
+  await page.locator('input[name="marriageDTO.wifeDTO.lastNameEng"]').click();
+  await page.locator('input[name="marriageDTO.wifeDTO.lastNameEng"]').fill('Koshti');
+  await page.locator('input[name="marriageDTO.wifeDTO.lastNameEng"]').click();
+  await page.locator('input[name="marriageDTO.wifeDTO.lastNameEng"]').press('ArrowLeft');
+
+  await page.locator('input[name="marriageDTO.wifeDTO.uidNo"]').click();
+  await page.locator('input[name="marriageDTO.wifeDTO.uidNo"]').fill('659089803220');
+  await page.locator('#wdob').click();
+  await page.getByRole('combobox').nth(3).selectOption('1989');
+  await page.locator('#wdob').click();
+  await page.getByRole('combobox').nth(3).selectOption('1997');
+  await page.getByRole('link', { name: '1', exact: true }).click();
+  await page.locator('a').filter({ hasText: 'Select' }).nth(1).click();
+  await page.getByRole('listitem').filter({ hasText: 'Hindu' }).click();
+  await page.locator('a').filter({ hasText: 'Select' }).nth(2).click();
+  await page.getByRole('listitem').filter({ hasText: 'Not Stated' }).nth(1).click();
+  await page.locator('a').filter({ hasText: 'Select' }).nth(2).click();
+  await page.getByRole('listitem').filter({ hasText: 'Unmarried' }).click();
+  await page.locator('textarea[name="marriageDTO.wifeDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="marriageDTO.wifeDTO.fullAddrEng"]').fill('Andheri');
+  await page.locator('textarea[name="marriageDTO.wifeDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="marriageDTO.wifeDTO.fullAddrEng"]').press('ArrowLeft');
+  await page.getByRole('button', { name: 'Save & Next' }).click();
+
+
+  await page.locator('input[name="witnessDTO.firstNameEng"]').click();
+  await page.locator('input[name="witnessDTO.firstNameEng"]').fill('Shivam');
+  await page.locator('input[name="witnessDTO.firstNameEng"]').click();
+  await page.locator('input[name="witnessDTO.firstNameEng"]').press('ArrowLeft');
+  await page.locator('input[name="witnessDTO.middleNameEng"]').click();
+  await page.locator('input[name="witnessDTO.middleNameEng"]').fill('Rajesh');
+  await page.locator('input[name="witnessDTO.middleNameEng"]').click();
+  await page.locator('input[name="witnessDTO.middleNameEng"]').press('ArrowLeft');
+  await page.locator('input[name="witnessDTO.lastNameEng"]').click();
+  await page.locator('input[name="witnessDTO.lastNameEng"]').fill('Koshti');
+  await page.locator('input[name="witnessDTO.lastNameEng"]').click();
+  await page.locator('input[name="witnessDTO.lastNameEng"]').press('ArrowLeft');
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').fill('Andheri');
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').press('ArrowLeft');
+  await page.locator('a').filter({ hasText: 'Select' }).nth(2).click();
+  await page.getByRole('listitem').filter({ hasText: 'Professional, Technical And' }).click();
+  await page.locator('#relation_chosen a').filter({ hasText: 'Select' }).click();
+  await page.locator('#relation_chosen').getByText('Friend').click();
+  await page.locator('input[name="witnessDTO.uidNo"]').click();
+  await page.locator('input[name="witnessDTO.uidNo"]').fill('272674504381');
+  await page.getByRole('button', { name: 'Add Witness' }).click();
+
+
+  await page.locator('input[name="witnessDTO.firstNameEng"]').click();
+  await page.locator('input[name="witnessDTO.firstNameEng"]').fill('Arpit');
+  await page.locator('input[name="witnessDTO.firstNameEng"]').click();
+  await page.locator('input[name="witnessDTO.firstNameEng"]').press('ArrowLeft');
+  await page.locator('input[name="witnessDTO.middleNameEng"]').click();
+  await page.locator('input[name="witnessDTO.middleNameEng"]').fill('Michael');
+  await page.locator('input[name="witnessDTO.middleNameEng"]').click();
+  await page.locator('input[name="witnessDTO.middleNameEng"]').press('ArrowLeft');
+  await page.locator('input[name="witnessDTO.lastNameEng"]').click();
+  await page.locator('input[name="witnessDTO.lastNameEng"]').fill('Jhon');
+  await page.locator('input[name="witnessDTO.lastNameEng"]').click();
+  await page.locator('input[name="witnessDTO.lastNameEng"]').press('ArrowLeft');
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').fill('Andheri');
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').press('ArrowLeft');
+  await page.locator('a').filter({ hasText: 'Select' }).nth(2).click();
+  await page.getByRole('listitem').filter({ hasText: 'Not Stated' }).click();
+  await page.locator('#relation_chosen a').filter({ hasText: 'Select' }).click();
+  await page.locator('#relation_chosen').getByText('Friend').click();
+  await page.locator('input[name="witnessDTO.uidNo"]').click();
+  await page.locator('input[name="witnessDTO.uidNo"]').fill('642273711014');
+  await page.getByRole('button', { name: 'Add Witness' }).click();
+  
+  await page.locator('input[name="witnessDTO.firstNameEng"]').click();
+  await page.locator('input[name="witnessDTO.firstNameEng"]').fill('Saroj');
+  await page.locator('input[name="witnessDTO.firstNameEng"]').click();
+  await page.locator('input[name="witnessDTO.firstNameEng"]').press('ArrowLeft');
+  await page.locator('input[name="witnessDTO.lastNameEng"]').click();
+  await page.locator('input[name="witnessDTO.lastNameEng"]').fill('Tambe');
+  await page.locator('input[name="witnessDTO.lastNameEng"]').click();
+  await page.locator('input[name="witnessDTO.lastNameEng"]').press('ArrowLeft');
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').fill('Mahim');
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').click();
+  await page.locator('textarea[name="witnessDTO.fullAddrEng"]').press('ArrowLeft');
+  await page.locator('a').filter({ hasText: 'Select' }).nth(2).click();
+  await page.getByRole('listitem').filter({ hasText: 'Professional, Technical And' }).click();
+  await page.locator('#relation_chosen a').filter({ hasText: 'Select' }).click();
+  await page.locator('#relation_chosen').getByText('Friend').click();
+  await page.locator('input[name="witnessDTO.uidNo"]').click();
+  await page.locator('input[name="witnessDTO.uidNo"]').fill('654216561137');
+  await page.getByRole('button', { name: 'Add Witness' }).click();
+  
+  
+  await page.getByRole('button', { name: 'Confirm to Proceed' }).click();
   const uploadContainer = page.locator('#file_list_0');
   await page.locator('#checkList0').setInputFiles(selectFile('sample file.pdf'));
   await expect(uploadContainer.getByText('File uploaded successfully')).toBeVisible();
@@ -68,9 +245,9 @@ test('MarriageRegistration', async ({ page }) => {
   await expect(uploadContainer10.getByText('File uploaded successfully')).toBeVisible();
   await expect(uploadContainer10.getByText('sample file 10.pdf')).toBeVisible();
 
-  await page.getByRole('button', { name: 'Confirm to Proceed' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByRole('button', { name: 'Yes' }).click();
-  const page1 = await page1Promise;
 
+  await page.getByRole('button', { name: 'Proceed' }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.locator('#Proceed').click();
+  const page1 = await page1Promise;
 });
