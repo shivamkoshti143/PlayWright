a2cd79d6a18fef4e32228a9e330123bc50642563
shivamkoshti-abm <shivam.koshti@abmindia.com>
2026-06-05 11:14:39 +0530

    RIghtToService

---
+++ SHIVAM_PLAYWRIGHT_SCRIPT/SKDCL/SKDCL_PORTAL/RightToService/tests/ExtractOfProperty.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test';
+import { doLogin, selectFile } from './Login.spec';
+
+test('ExtractOfProperty', async ({ page }) => {
+  test.setTimeout(0);
+  // Read Excel
+
+  await doLogin(page);
+
+  await page.getByRole('link', { name: 'Citizen Services' }).click();
+  await page.getByRole('link', { name: 'Right to Service ' }).click();
+  await page.getByRole('link', { name: 'Extract of Property' }).click();
+  await page.getByRole('textbox', { name: ': First Name *' }).click();
+  await page.getByRole('textbox', { name: ': First Name *' }).fill('Shivam');
+  await page.getByRole('textbox', { name: ': Last Name *' }).click();
+  await page.getByRole('textbox', { name: ': Last Name *' }).fill('Koshti');
+  await page.getByLabel('DIvision').selectOption('768');
+  await page.getByLabel('Ward').selectOption('770');
+  await page.locator('#finacialYearId0').selectOption('100000044');
+  await page.locator('input[name="noDuesCertificateDto.propertyDetails[0].propNo"]').click();
+  await page.locator('input[name="noDuesCertificateDto.propertyDetails[0].propNo"]').fill('A01004918000');
+  await page.locator('#ownerName0').click();
+
+  await page.getByRole('button', { name: 'Proceed' }).click();
+  const uploadContainer = page.locator('#file_list_0');
+  await page.locator('#checkList0').setInputFiles(selectFile('sample file.pdf'));
+  await expect(uploadContainer.getByText('File uploaded successfully')).toBeVisible();
+  await expect(uploadContainer.getByText('sample file.pdf')).toBeVisible();
+
+  await page.getByRole('button', { name: 'Save Application' }).click();
+  await page.getByRole('button', { name: 'Proceed' }).click();
+
+  await page.locator('#paymobile2').click();
+  await page.locator('#paymobile2').fill('9157285139');
+
+
+  await page.locator('#payeemail2').click();
+  await page.locator('#payeemail2').fill('sample@gmail.com');
+
+
+  await page.locator('#cbBankid').selectOption('2');
+  await page.getByRole('button', { name: 'Pay' }).click();
+  await page.locator('#tab-4').check();
+  await page.locator('#citrusAvailableOptions').selectOption('2001:000000');
+
+  const [paymentPage] = await Promise.all([
+    await page.getByRole('button', { name: 'Make Payment' }).click(),
+
+    await page.getByRole('textbox', { name: 'Mobile Number' }).click(),
+    await page.getByRole('textbox', { name: 'Mobile Number' }).fill('9157285139'),
+    await page.getByRole('textbox', { name: 'Email Id' }).click(),
+    await page.getByRole('textbox', { name: 'Email Id' }).fill('sample@gmail.com'),
+    await page.locator('#bankID').selectOption('2001'),
+    await page.getByRole('button', { name: 'Pay Now' }).click(),
+    await page.getByRole('button', { name: 'Click To Complete Transaction' }).click(),
+
+  ]);
+
+});
---
+++ SHIVAM_PLAYWRIGHT_SCRIPT/SKDCL/SKDCL_PORTAL/RightToService/tests/PropertyOwnershipTransfer.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+import { doLogin, selectFile } from './Login.spec';
+
+test('PropertyOwnershipTransfer', async ({ page }) => {
+  test.setTimeout(0);
+  // Read Excel
+
+  await doLogin(page);
+
+  await page.getByRole('link', { name: 'Citizen Services' }).click();
+  await page.getByRole('link', { name: 'Right to Service ' }).click();
+  await page.getByRole('link', { name: 'Property Ownership Transfer' }).click();
+  await page.locator('#assNo').click();
+  await page.locator('#assNo').fill('A01005045400');
+  await page.getByRole('button', { name: ' Search' }).click();
+
+  await page.locator('[id="propTransferDto.transferType"]').selectOption('1049');
+  await page.getByRole('textbox', { name: 'DD/MM/YYYY' }).click();
+  await page.getByRole('link', { name: '1', exact: true }).click();
+  await page.locator('#ownerInfo').selectOption('949');
+  await page.locator('#assoOwnerName').click();
+  await page.locator('#assoOwnerName').fill('Shivam Koshti');
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].assoOwnerNameReg"]').click();
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].assoOwnerNameReg"]').fill('Shivam');
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].mobileno"]').click();
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].mobileno"]').fill('9157285139');
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].eMail"]').click();
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].eMail"]').fill('shivamkoshti1@gmail.com');
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].addharno"]').click();
+  await page.locator('input[name="propTransferDto.propTransferOwnerList[0].addharno"]').fill('979812345678');
+  await page.locator('#pannumber').click();
+  await page.locator('#pannumber').fill('FHCPK0167G');
+  await page.getByRole('button', { name: 'Proceed' }).click();
+  const uploadContainer = page.locator('#file_list_0');
+  await page.locator('#checkList0').setInputFiles(selectFile('sample file.pdf'));
+  await expect(uploadContainer.getByText('File uploaded successfully')).toBeVisible();
+  await expect(uploadContainer.getByText('sample file.pdf')).toBeVisible();
+
+  await page.getByRole('button', { name: 'save Mutation' }).click();
+  await page.getByRole('button', { name: 'Proceed' }).click();
+});
